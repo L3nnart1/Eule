41134fe029fcaa08298cde0f4e8ec2682ad29cdf
L3nnart1 <lobitz.business@gmail.com>
2023-02-22 22:36:11 +0100

    btn fix

--- eule/script.js
+++ eule/script.js
@@ -1,4 +1,12 @@
 const buy = document.querySelector(".sold");
+const btn = document.querySelector("#btn");
+const close = document.querySelector("#close");
 function sell(){
     buy.classList.toggle("open");
 }
+btn.addEventListener("pointerdown", () => {
+    buy.classList.toggle("open")
+})
+close.addEventListener("pointerdown", () => {
+    buy.classList.toggle("open")
+})
